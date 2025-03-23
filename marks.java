c30d4b19a7fd13495ffa354150ec52e7da2c9606
ParthibBiswas10 <parthiblm10@gmail.com>
2025-03-23 12:30:24 +0530

    New files

---
+++ marks.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class marks {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int n = sc.nextInt();
+        int marks[] = new int[n];
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            System.out.print("Enter score of " + (i + 1) + " Subject: ");
+            marks[i] = sc.nextInt();
+            total = total + marks[i];
+        }
+        float avg = (float) total / n;
+        System.out.print("The average Score is: " + avg);
+    }
+}
