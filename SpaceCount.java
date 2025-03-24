1d11500940f46d720526ca4c5dc1163fa2758114
ParthibBiswas10 <parthiblm10@gmail.com>
2025-03-24 11:59:08 +0530

    New files

--- SpaceCount.java
+++ SpaceCount.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class SpaceCount {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String Str=sc.nextLine();
+        int SpaceCount=count(Str," ");
+        int DoubleSpaceCount=count(Str,"  ");
+        int TripleSpaceCount=count(Str,"   ");
+        System.out.println("Number of Single Space: "+SpaceCount);
+        System.out.println("Number of Double Space: "+DoubleSpaceCount);
+        System.out.println("Number of Triple Space: "+TripleSpaceCount);
+        sc.close();
+    }
+   public static int count(String str,String subStr){
+        int Count=0;
+        int index=0;
+        while((index= str.indexOf(subStr))!=-1){
+            Count++;
+            System.out.println(Count);
+            index+=subStr.length();
+
+        }
+        return Count;
+
+   }
 }
