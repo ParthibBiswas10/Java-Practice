182bc832a3b2e01b0fc8c13bc16eb9bfabdb1b49
ParthibBiswas10 <parthiblm10@gmail.com>
2025-04-19 11:13:21 +0530

    new

--- StrongNumber.java
+++ StrongNumber.java
@@ -1,12 +1,22 @@
 import java.util.*;
 public class StrongNumber {
+    public static int fact(int num){
+        int a=1;
+        for(int i=1;i<=n;i++){
+            a=a*i;
+        }
+        return a;
+    }
+
     public static int check(int n){
         int copy=n;
+        int sum=0;
         while(copy>0){
-            fact(copy%10);
-            
+            sum=fact(copy%10)+sum;
+            copy=copy/10;
 
         }
+        return sum;
 
     }
     public static void main(String[] args) {
@@ -14,7 +24,11 @@ public class StrongNumber {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter NUmber: ");
         n=sc.nextInt();
-        check(n);
+        if(n==check(n)){
+            System.out.println("Strong Number");
+        }
+        else
+            System.out.println("Not Strong number");
 
 
     }
