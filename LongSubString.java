9eb31d665caafe67bc8fde1e39bc6dd81baf7604
ParthibBiswas10 <parthiblm10@gmail.com>
2025-04-24 17:34:03 +0530

    new

---
+++ LongSubString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LongSubString {
+    public static int check(string s){
+        
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int result=check(str);
+
+    }
+}
