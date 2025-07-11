1f8cfd11e306af7b08091917da574425dee8fbff
ParthibBiswas10 <parthiblm10@gmail.com>
2025-07-12 00:04:33 +0530

    Happy NUmber

---
+++ Practice/HappyDigit.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        int res = 0;
+        int temp = n;
+        
+        while(temp != 1 && !seen.contains(temp)) {
+            seen.add(temp);
+            res = 0;
+            while(temp > 0) {
+                int rem = temp % 10;
+                res = res + (int) Math.pow(rem, 2);
+                temp = temp / 10;
+            }
+            temp = res;
+        }
+        return temp == 1;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        
+        Solution solution = new Solution();
+        boolean result = solution.isHappy(n);
+        
+        System.out.println(n + " is " + (result ? "happy" : "not happy"));
+    }
+}
