915fe3d208ef99cc5b50ba2ba2cd99ec6af95ec3
ParthibBiswas10 <parthiblm10@gmail.com>
2025-08-29 00:45:31 +0530

    Third Max NUmber

---
+++ IMPS/414ThirdMaxNUmber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+     
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+   
+        if (set.size() < 3) {
+            return Collections.max(set);
+        }
+
+   
+        set.remove(Collections.max(set));  
+        set.remove(Collections.max(set)); 
+
+ 
+        return Collections.max(set);
+    }
+}
