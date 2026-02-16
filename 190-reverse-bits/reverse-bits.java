7d7cd59ef73a183fa66ff95cb8362e4b02a6ea13
Parthib Biswas <162007516+ParthibBiswas10@users.noreply.github.com>
2026-02-16 11:56:22 +0530

    Time: 1 ms (60.34%) | Memory: 42.3 MB (85.32%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -3,8 +3,10 @@ class Solution {
         int result = 0;
 
         for (int i = 0; i < 32; i++) {
-            result = (result << 1) | (n & 1);
-            n >>= 1;
+           int a=n&1;
+           result<<=1;
+           result=result|a;
+           n>>=1;
         }
 
         return result;
