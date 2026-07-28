57a55df10d3096bc76c8cd123419b499da64a467
Parthib Biswas <162007516+ParthibBiswas10@users.noreply.github.com>
2026-07-28 11:23:23 +0530

    Time: 32 ms (53.82%) | Memory: 48.1 MB (57.43%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -3,10 +3,43 @@ class Solution {
         int n = s.length();
         int mid = n / 2;
         char[] chars = s.toCharArray();
-        Arrays.sort(chars, 0, mid); // note: mid is not included i.e. [0, mid)
+        Arrays.sort(chars, 0, mid); 
         for (int i = 0; i < mid; i++) {
             chars[n - 1 - i] = chars[i];
         }
         return new String(chars);
     }
 }
+
+
+/*
+    my solution: 
+        class Solution {
+    public String smallestPalindrome(String s) {
+        int n = s.length();
+        int mid = n / 2;
+
+        // Take the first half
+        char[] first = s.substring(0, mid).toCharArray();
+
+        // Sort it
+        Arrays.sort(first);
+
+        // Build the first half
+        String firstHalf = new String(first);
+
+        // Reverse it to get the second half
+        String secondHalf = new StringBuilder(firstHalf).reverse().toString();
+
+        // Handle the middle character (only for odd length)
+        if (n % 2 == 1) {
+            return firstHalf + s.charAt(mid) + secondHalf;
+        }
+
+        return firstHalf + secondHalf;
+    }
+}
+
+
+
+ */
